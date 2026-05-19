2d4c0f0bb45bc16ab1e07cd677838ec95727d7cc
palomitadkaramelo <palomitadkaramelo@gmail.com>
2026-05-19 14:13:22 -0600

    Create Problema1.c

---
+++ Problema1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int cont=0, i;
+    char respuesta; 
+    
+    printf("====visualizacion de los numeros comprendidos entre el 1 y 1000====");
+    for(i=1; i<1000; i++ );
+    {
+    printf("%d", i );
+    cont ++;
+    
+    if(cont==20)
+    {
+        printf("Deseas continuar");
+        scanf("%c",respuesta);
+    if(respuesta== "n"|| respuesta=  );
+    {
+        break;
+    }
+    contador=0;
+    }
+    return 0;
+}
